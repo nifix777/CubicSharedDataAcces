db5e5a42dea67f99a849245a366c940ec25c3424
nifix777 <nifix777@gmail.com>
2020-11-05 00:30:25 +0100

    renamed internal reader field

--- src/Cubic.Shared.Data.Core/Recordset.cs
+++ src/Cubic.Shared.Data.Core/Recordset.cs
@@ -12,7 +12,7 @@ namespace Cubic.Shared.Data.Core
 
     private DataSet data;
 
-    private DbDataReader reader;
+    private DbDataReader tableReader;
     public Recordset(IDataAdapter dataAdapter)
     {
       _dataAdapter = dataAdapter ?? throw new ArgumentNullException(nameof(dataAdapter));
@@ -24,151 +24,151 @@ namespace Cubic.Shared.Data.Core
 
     #region IDataReader
 
-    public object this[int i] => ((IDataRecord)reader)[i];
+    public object this[int i] => ((IDataRecord)tableReader)[i];
 
-    public object this[string name] => ((IDataRecord)reader)[name];
+    public object this[string name] => ((IDataRecord)tableReader)[name];
 
-    public int Depth => ((IDataReader)reader).Depth;
+    public int Depth => ((IDataReader)tableReader).Depth;
 
-    public bool IsClosed => ((IDataReader)reader).IsClosed;
+    public bool IsClosed => ((IDataReader)tableReader).IsClosed;
 
-    public int RecordsAffected => ((IDataReader)reader).RecordsAffected;
+    public int RecordsAffected => ((IDataReader)tableReader).RecordsAffected;
 
-    public int FieldCount => ((IDataRecord)reader).FieldCount;
+    public int FieldCount => ((IDataRecord)tableReader).FieldCount;
 
     public void Close()
     {
-      ((IDataReader)reader).Close();
+      ((IDataReader)tableReader).Close();
     }
 
     public void Dispose()
     {
-      ((IDisposable)reader).Dispose();
+      ((IDisposable)tableReader).Dispose();
     }
 
     public bool GetBoolean(int i)
     {
-      return ((IDataRecord)reader).GetBoolean(i);
+      return ((IDataRecord)tableReader).GetBoolean(i);
     }
 
     public byte GetByte(int i)
     {
-      return ((IDataRecord)reader).GetByte(i);
+      return ((IDataRecord)tableReader).GetByte(i);
     }
 
     public long GetBytes(int i, long fieldOffset, byte[] buffer, int bufferoffset, int length)
     {
-      return ((IDataRecord)reader).GetBytes(i, fieldOffset, buffer, bufferoffset, length);
+      return ((IDataRecord)tableReader).GetBytes(i, fieldOffset, buffer, bufferoffset, length);
     }
 
     public char GetChar(int i)
     {
-      return ((IDataRecord)reader).GetChar(i);
+      return ((IDataRecord)tableReader).GetChar(i);
     }
 
     public long GetChars(int i, long fieldoffset, char[] buffer, int bufferoffset, int length)
     {
-      return ((IDataRecord)reader).GetChars(i, fieldoffset, buffer, bufferoffset, length);
+      return ((IDataRecord)tableReader).GetChars(i, fieldoffset, buffer, bufferoffset, length);
     }
 
     public IDataReader GetData(int i)
     {
-      return ((IDataRecord)reader).GetData(i);
+      return ((IDataRecord)tableReader).GetData(i);
     }
 
     public string GetDataTypeName(int i)
     {
-      return ((IDataRecord)reader).GetDataTypeName(i);
+      return ((IDataRecord)tableReader).GetDataTypeName(i);
     }
 
     public DateTime GetDateTime(int i)
     {
-      return ((IDataRecord)reader).GetDateTime(i);
+      return ((IDataRecord)tableReader).GetDateTime(i);
     }
 
     public decimal GetDecimal(int i)
     {
-      return ((IDataRecord)reader).GetDecimal(i);
+      return ((IDataRecord)tableReader).GetDecimal(i);
     }
 
     public double GetDouble(int i)
     {
-      return ((IDataRecord)reader).GetDouble(i);
+      return ((IDataRecord)tableReader).GetDouble(i);
     }
 
     public Type GetFieldType(int i)
     {
-      return ((IDataRecord)reader).GetFieldType(i);
+      return ((IDataRecord)tableReader).GetFieldType(i);
     }
 
     public float GetFloat(int i)
     {
-      return ((IDataRecord)reader).GetFloat(i);
+      return ((IDataRecord)tableReader).GetFloat(i);
     }
 
     public Guid GetGuid(int i)
     {
-      return ((IDataRecord)reader).GetGuid(i);
+      return ((IDataRecord)tableReader).GetGuid(i);
     }
 
     public short GetInt16(int i)
     {
-      return ((IDataRecord)reader).GetInt16(i);
+      return ((IDataRecord)tableReader).GetInt16(i);
     }
 
     public int GetInt32(int i)
     {
-      return ((IDataRecord)reader).GetInt32(i);
+      return ((IDataRecord)tableReader).GetInt32(i);
     }
 
     public long GetInt64(int i)
     {
-      return ((IDataRecord)reader).GetInt64(i);
+      return ((IDataRecord)tableReader).GetInt64(i);
     }
 
     public string GetName(int i)
     {
-      return ((IDataRecord)reader).GetName(i);
+      return ((IDataRecord)tableReader).GetName(i);
     }
 
     public int GetOrdinal(string name)
     {
-      return ((IDataRecord)reader).GetOrdinal(name);
+      return ((IDataRecord)tableReader).GetOrdinal(name);
     }
 
     public DataTable GetSchemaTable()
     {
-      return ((IDataReader)reader).GetSchemaTable();
+      return ((IDataReader)tableReader).GetSchemaTable();
     }
 
     public string GetString(int i)
     {
-      return ((IDataRecord)reader).GetString(i);
+      return ((IDataRecord)tableReader).GetString(i);
     }
 
     public object GetValue(int i)
     {
-      return ((IDataRecord)reader).GetValue(i);
+      return ((IDataRecord)tableReader).GetValue(i);
     }
 
     public int GetValues(object[] values)
     {
-      return ((IDataRecord)reader).GetValues(values);
+      return ((IDataRecord)tableReader).GetValues(values);
     }
 
     public bool IsDBNull(int i)
     {
-      return ((IDataRecord)reader).IsDBNull(i);
+      return ((IDataRecord)tableReader).IsDBNull(i);
     }
 
     public bool NextResult()
     {
-      return ((IDataReader)reader).NextResult();
+      return ((IDataReader)tableReader).NextResult();
     }
 
     public bool Read()
     {
-      return ((IDataReader)reader).Read();
+      return ((IDataReader)tableReader).Read();
     }
 
     #endregion
@@ -176,7 +176,7 @@ namespace Cubic.Shared.Data.Core
     public void Fill(IDataReader reader)
     {
       Table.Load(reader);
-      reader = Table.CreateDataReader();
+      tableReader = Table.CreateDataReader();
     }
     public int Update()
     {
